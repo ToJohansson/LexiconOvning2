3897dc669c3f7c7e92173245874594c0ecceffc5
Tobias Johansson <tobias@noq.nu>
2025-04-04 09:56:01 +0200

    refactored main menu

--- LexiconOvning2/MainMenu.cs
+++ LexiconOvning2/MainMenu.cs
@@ -4,57 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LexiconOvning2
+namespace LexiconOvning2;
+
+
+internal class MainMenu
 {
-    internal class MainMenu
+    /// <summary>
+    /// This class contains the main menu for the program, allowing users to choose between different functions.
+    /// </summary>
+    public static void Run()
     {
-        public static void Run()
+
+        string mainMenuGreeting = "Welcome to the main menu! \n" +
+            "You will now get a few options to test out some functions. \n" +
+            "To toggle in the menu, press the correct number to active the desired function. \n" +
+            "\n" +
+            "### MAIN MENU ### \n" +
+            "1: Cinema \n" +
+            "2: Repeat message\n" +
+            "3: The third word\n" +
+            "4: Working with stringbuilder!\n" +
+            "0: Exit program";
+
+        bool isRunning = true;
+        while (isRunning)
         {
+            Console.Clear();
+            Console.WriteLine(mainMenuGreeting);
 
-            string mainMenuGreeting = "Welcome to the main menu! \n" +
-                "You will now get a few options to test out some functions. \n" +
-                "To toggle in the menu, press the correct number to active the desired function. \n" +
-                "\n" +
-                "### MAIN MENU ### \n" +
-                "1: Cinema \n" +
-                "2: Repeat message\n" +
-                "3: The third word\n" +
-                "4: Working with stringbuilder!\n" +
-                "0: Exit program";
-
-            bool isRunning = true;
-            while (isRunning)
+            string option = Console.ReadLine();
+            switch (option)
             {
-                Console.Clear();
-                Console.WriteLine(mainMenuGreeting);
-
-                string option = Console.ReadLine();
-                switch (option)
-                {
-                    case "1":
-                        Cinema.Menu();
-                        break;
-                    case "2":
-                        Repeat.Menu();
-                        break;
-                    case "3":
-                        ThirdWord.Run();
-                        break;
-                    case "4":
-                        WorkingWithStringBuilder.Menu();
-                        break;
-                    case "0":
-                        Console.WriteLine("Exiting program...");
-                        isRunning = false;
-                        break;
-                    default:
-                        Console.WriteLine("Wrong input, please try again.");
-                        break;
-                }
-                Console.ReadLine();
-
+                case "1":
+                    Cinema.Menu();
+                    break;
+                case "2":
+                    Repeat.Menu();
+                    break;
+                case "3":
+                    ThirdWord.Run();
+                    break;
+                case "4":
+                    WorkingWithStringBuilder.Menu();
+                    break;
+                case "0":
+                    Console.WriteLine("Exiting program...");
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Wrong input, please try again.");
+                    break;
             }
+            Console.ReadLine();
 
         }
+
     }
 }
